7fa051f018a4a1264c81fb14df82c9a5754a0789
trainig2020 <68064760+trainig2020@users.noreply.github.com>
2020-07-31 15:00:04 +0530

    Add files via upload

---
+++ springSecurityUsingJPA/src/main/java/com/springSecurityUsingJPA/SpringSecurityUsingJpaApplication.java
@@ -0,0 +1,13 @@
+package com.springSecurityUsingJPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityUsingJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityUsingJpaApplication.class, args);
+	}
+
+}
